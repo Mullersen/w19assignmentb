1eb9d39c7c00c84f980670d93d3bddd4678790b0
mullersen <mariannhaugland@gmail.com>
2020-03-13 03:11:41 +0000

    added on click events to buttons

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,24 +1,57 @@
 <template>
-    <div id="body">
-        <button id="buttonOne">This is the first button</button>
-        <button id="buttonOne">This is the second button</button>
-        <button id="buttonOne">This is the third button</button>
+<div id="body-container">
+    <div class="body">
+        <button v-on:click="hello" id="buttonOne">This is the first button</button>
     </div>
+    <div class="body">
+        <button v-on:click="background" id="buttonTwo">This is the second button</button>
+    </div>
+    <div class="body">
+        <button v-on:click="move" id="buttonThree">This is the third button</button>
+    </div>
+</div>
 </template>
 
 <script>
+
 export default {
     name: 'myBody',
     props: {
         msg: String
+    },
+    methods: {
+        hello: function(){
+            var newChild = document.createElement('p');
+            var content = document.createTextNode('Hello there, you look nice today!');
+            newChild.appendChild(content);
+            document.getElementById('body-container').appendChild(newChild);
+        },
+        background: function(){
+            document.getElementById('body-container').style.backgroundColor= "green";
+        },
+        move: function(){
+            var marginCounter = 0;
+            var Int = setInterval(function() {
+                    console.log(marginCounter);
+                    document.getElementById('buttonThree').style.marginTop = marginCounter + "px";
+                    marginCounter += 0.1;
+                    if (marginCounter >= 100) {
+                        clearInterval(Int);
+                    }
+                }, 10);
+        }
     }
 }
 </script>
 
 <style scoped>
-#body{ 
+#body-container{ 
     height: 80vh;
     width:100%;
     border-bottom: solid 1px grey;
+}
+.body{
+    height: 20vh;
+    width: 50%;
 }
 </style>
